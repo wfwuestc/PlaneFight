9da0c04d53dc90901c1cdb287b47538a39abd4f8
Wen <wfwuestc@163.com>
2018-01-17 21:48:06 +0800

    将scene 改为类

---
+++ guagame/gua_image.js
@@ -0,0 +1,30 @@
+class GuaImage {
+  constructor(game, name) {
+    // this.game = game
+    this.texture = game.textureByName(name)
+    this.x = 0
+    this.y = 0
+    this.w = this.texture.width
+    this.h = this.texture.height
+  }
+
+  static new(...args) {
+    var i = new this(...args)
+    return i
+  }
+
+  draw() {
+
+  }
+
+  update() {
+
+  }
+}
+
+class Player extends GuaImage {
+  constructor(game, name) {
+    super(game, name)
+
+  }
+}
--- guagame/gua_scene.js
+++ guagame/gua_scene.js
@@ -1,15 +1,26 @@
 class GuaScene {
-    constructor(game) {
-        this.game = game
-    }
-    static new(game) {
-        var i = new this(game)
-        return i
-    }
-    draw() {
+  constructor(game) {
+    this.game = game
+    this.elements = []
+  }
 
+  static new(game) {
+    var i = new this(game)
+    return i
+  }
+  addElements(e) {
+    this.elements.push(e)
+  }
+  draw() {
+    log('1111')
+    for (var i = 0; i<this.elements.length; i++){
+      var e = this.elements[i]
+      log(e,'1')
+      this.game.drawImage(e)
     }
-    update() {
+  }
 
-    }
+  update() {
+
+  }
 }
